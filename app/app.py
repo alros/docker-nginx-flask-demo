d84f7976e715ea0d68c8e3b900bbad408e68f7d7
Yusuf Tunc <wyusuftunc@gmail.com>
2023-11-26 17:14:45 +0300

    Webhook endpoint added.

--- app/app.py
+++ app/app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, request
+import requests
+from datetime import datetime
+
+BRAIN_URL = "http://brain.vape-jet.com"
+BRAIN_WEBHOOK_URL = f"{BRAIN_URL}/webhook/atlassian"
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "hello world!"
-    
+def index():
+    return "OK"
+
+
+@app.route("/api/webhook/atlassian", methods=["POST"])
+def atlassian_webhook():
+    try:
+        prefix = f"[{datetime.now().isoformat()}] - JIRA"
+        print(f"{prefix} - {request.json}")
+
+        response = requests.post(BRAIN_WEBHOOK_URL, json=request.json)
+        response.raise_for_status()
+
+        return "OK", 200
+    except Exception as e:
+        app.logger.error(e)
+        return "ERROR", 500
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=80)
